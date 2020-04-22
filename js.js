0e343f913ac78c457f97fa256abfb5a63bf10ff1
Jepression <koipression@gmail.com>
2020-04-22 11:39:21 +0700

    new
    Загрузка сайта в репозиторий

---
+++ js.js
@@ -0,0 +1,45 @@
+$('.slider_for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider_nav'
+  });
+
+$('.slider_nav').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider_for',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
---
+++ popup.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function(){
+    PopUpHide();
+	PopUpHide2();
+});
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
+function PopUpShow2(){
+    $("#popup2").show();
+}
+function PopUpHide2(){
+    $("#popup2").hide();
+}
